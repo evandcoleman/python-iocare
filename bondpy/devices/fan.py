11c76d8bf37d3c36a4c96d30269cbb61b72edbd1
Evan Coleman <e@edc.me>
2018-08-21 19:49:35 -0400

    Initial commit

--- bondpy/devices/fan.py
+++ bondpy/devices/fan.py
@@ -1,18 +1,20 @@
+import collections
+
 from bondpy.devices.base import BondDevice
 
 class BondFanDevice(BondDevice):
 
     def speed_list(self):
-        return list(filter(lambda x: x.name().startswith('Speed'), self.commands))
+        return list(filter(lambda x: x.name().startswith('Speed'), self.commands()))
 
     def set_speed(self, speed):
         self.api.device_control(self.obj_id, speed.command_id())
 
     def reverse(self):
-        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Reverse', self.commands))[0])
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Reverse', self.commands()))[0])
 
     def support_reverse(self):
-        return len(list(filter(lambda x: x.name() == 'Reverse', self.commands))) > 0
+        return len(list(filter(lambda x: x.name() == 'Reverse', self.commands()))) > 0
 
 
 
--- build/lib/bondpy/devices/base.py
+++ build/lib/bondpy/devices/base.py
@@ -3,14 +3,14 @@ import collections
 
 class BondCommand(object):
     def __init__(self, data):
-        self.obj_id = data.get('device_property_command_id')
-        self.obj_name = data.get('command_type')
+        self.id = data.get('device_property_command_id')
+        self.name = data.get('command_type')
 
     def command_id(self):
-        return self.obj_id
+        return self.id
 
     def name(self):
-        return self.obj_name
+        return self.name
 
 class BondDevice(object):
     def __init__(self, data, api):
@@ -19,13 +19,13 @@ class BondDevice(object):
         self.obj_id = data[0].get('device_property_id')
         self.obj_name = data[0].get('location_type') + ' ' + data[0].get('device_type')
         self.dev_type = data[0].get('device_type')
-        self.obj_commands = list(map(lambda x: BondCommand(x), data))
+        self.commands = list(map(lambda x: BondCommand(x), data))
 
     def name(self):
         return self.obj_name
 
     def commands(self):
-        return self.obj_commands
+        return self.commands
 
     def device_type(self):
         return self.dev_type
