632c617594a7d865ceb9102762c29da4c5067474
Evan Coleman <e@edc.me>
2018-08-21 21:58:05 -0400

    add power

--- bondpy/devices/fan.py
+++ bondpy/devices/fan.py
@@ -4,6 +4,9 @@ from bondpy.devices.base import BondDevice
 
 class BondFanDevice(BondDevice):
 
+    def power_toggle(self):
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Power Toggle', self.commands()))[0])
+
     def speed_list(self):
         return list(filter(lambda x: x.name().startswith('Speed'), self.commands()))
 
@@ -15,6 +18,3 @@ class BondFanDevice(BondDevice):
 
     def support_reverse(self):
         return len(list(filter(lambda x: x.name() == 'Reverse', self.commands()))) > 0
-
-
-
