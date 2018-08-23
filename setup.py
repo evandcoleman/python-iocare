81325445512c75d59008259b7fe03c86cf38e49e
Evan Coleman <e@edc.me>
2018-08-22 21:45:47 -0400

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bondpy",
-    version="0.1.4",
+    version="0.1.5",
     author="Evan Coleman",
     author_email="oss@edc.me",
     description="A Python library for the BOND API with Python 3+",
