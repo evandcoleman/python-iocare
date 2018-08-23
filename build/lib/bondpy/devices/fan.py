b5a59f51bf9e69bcb610b6c2b8a52522fe6c644f
Evan Coleman <e@edc.me>
2018-08-22 21:43:12 -0400

    bug fix

--- build/lib/bondpy/devices/fan.py
+++ build/lib/bondpy/devices/fan.py
@@ -5,16 +5,16 @@ from bondpy.devices.base import BondDevice
 class BondFanDevice(BondDevice):
 
     def power_toggle(self):
-        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Power Toggle', self.commands()))[0])
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Power Toggle', self.obj_commands))[0])
 
     def speed_list(self):
-        return list(filter(lambda x: x.name().startswith('Speed'), self.commands()))
+        return list(filter(lambda x: x.name().startswith('Speed'), self.obj_commands))
 
     def set_speed(self, speed):
         self.api.device_control(self.obj_id, speed.command_id())
 
     def reverse(self):
-        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Reverse', self.commands()))[0])
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name() == 'Reverse', self.obj_commands))[0])
 
     def support_reverse(self):
-        return len(list(filter(lambda x: x.name() == 'Reverse', self.commands()))) > 0
+        return len(list(filter(lambda x: x.name() == 'Reverse', self.obj_commands))) > 0
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bondpy",
-    version="0.1.3",
+    version="0.1.4",
     author="Evan Coleman",
     author_email="oss@edc.me",
     description="A Python library for the BOND API with Python 3+",
