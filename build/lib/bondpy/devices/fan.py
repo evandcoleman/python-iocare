35f1c94a8175845735833a76edf2bff7531e8cd7
Evan Coleman <e@edc.me>
2018-08-23 20:56:30 -0400

    bug fix

--- build/lib/bondpy/devices/fan.py
+++ build/lib/bondpy/devices/fan.py
@@ -5,7 +5,7 @@ from bondpy.devices.base import BondDevice
 class BondFanDevice(BondDevice):
 
     def power_toggle(self):
-        self.api.device_control(self.obj_id, list(filter(lambda x: x.name == 'Power Toggle', self.commands))[0].command_id)
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name == 'Power Toggle', self.commands))[0].command_id())
 
     def speed_list(self):
         return list(filter(lambda x: x.name.startswith('Speed'), self.commands))
@@ -14,7 +14,7 @@ class BondFanDevice(BondDevice):
         self.api.device_control(self.obj_id, speed.command_id)
 
     def reverse(self):
-        self.api.device_control(self.obj_id, list(filter(lambda x: x.name == 'Reverse', self.commands))[0].command_id)
+        self.api.device_control(self.obj_id, list(filter(lambda x: x.name == 'Reverse', self.commands))[0].command_id())
 
     def support_reverse(self):
         return len(list(filter(lambda x: x.name == 'Reverse', self.commands))) > 0
