a8bbd82cfb810b0d4fbc54b0e024263b3b6618af
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-04-26 14:58:04 -0400

    handle dict KeyError

--- iocare/devices/purifier.py
+++ iocare/devices/purifier.py
@@ -13,37 +13,40 @@ class Purifier(object):
 
     def refresh(self):
         control_status = self.api.control_status(self)
-        self.is_on = control_status['0001'] == '1'
-        self.is_auto = control_status['0002'] == '1'
-        self.is_auto_eco = control_status['0002'] == '6'
-        self.is_night = control_status['0002'] == '2'
-        self.fan_speed = control_status['0003']
-        self.is_light_on = control_status['0007'] == '2'
-        self.timer = control_status['offTimerData']
-        self.timer_remaining = control_status['0008']
-        filters, quality, iaq = self.api.quality_status(self)
-        fs = []
-        for f in filters:
-            fs.append({
-                'name': f['filterName'],
-                'life_level_pct': f['filterPer'],
-                'last_changed': f['lastChangeDate'],
-                'change_months': f['changeCycle']
-            })
-        self.filters = fs
-        self.quality = {}
-        if len(quality) > 0:
-            q = quality[0]
-            self.quality['dust_pollution'] = q['dustPollution']
-            self.quality['air_volume'] = q['airVolume']
-            self.quality['pollen_mode'] = q['pollenMode']
-        if len(iaq) > 0:
-            q = iaq[0]
-            self.quality['particulate_matter_2_5'] = q['dustpm25']
-            self.quality['particulate_matter_10'] = q['dustpm10']
-            self.quality['carbon_dioxide'] = q['co2']
-            self.quality['volatile_organic_compounds'] = q['vocs']
-            self.quality['air_quality_index'] = q['inairquality']
+        try:
+            self.is_on = control_status['0001'] == '1'
+            self.is_auto = control_status['0002'] == '1'
+            self.is_auto_eco = control_status['0002'] == '6'
+            self.is_night = control_status['0002'] == '2'
+            self.fan_speed = control_status['0003']
+            self.is_light_on = control_status['0007'] == '2'
+            self.timer = control_status['offTimerData']
+            self.timer_remaining = control_status['0008']
+            filters, quality, iaq = self.api.quality_status(self)
+            fs = []
+            for f in filters:
+                fs.append({
+                    'name': f['filterName'],
+                    'life_level_pct': f['filterPer'],
+                    'last_changed': f['lastChangeDate'],
+                    'change_months': f['changeCycle']
+                    })
+            self.filters = fs
+            self.quality = {}
+            if len(quality) > 0:
+                q = quality[0]
+                self.quality['dust_pollution'] = q['dustPollution']
+                self.quality['air_volume'] = q['airVolume']
+                self.quality['pollen_mode'] = q['pollenMode']
+            if len(iaq) > 0:
+                q = iaq[0]
+                self.quality['particulate_matter_2_5'] = q['dustpm25']
+                self.quality['particulate_matter_10'] = q['dustpm10']
+                self.quality['carbon_dioxide'] = q['co2']
+                self.quality['volatile_organic_compounds'] = q['vocs']
+                self.quality['air_quality_index'] = q['inairquality']
+        except KeyError as e:
+            print(e)
 
     def set_power(self, on):
         self.is_on = on
